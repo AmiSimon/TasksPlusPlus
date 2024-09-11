d4eb00f39565fe614f16fa9aa968434a92b42e08
Simon Leneveu <simon.leneveu@gmail.com>
2024-09-11 22:47:23 +0200

    initial commit

---
+++ taskPlusPlus.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Task {
+    public:
+        std::string description;
+        int importance;
+};
+
+int main() {
+    return 0;
+}
