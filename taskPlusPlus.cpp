28c7a94570a09e53fe91c0885fff55ac6f90920e
Simon Leneveu <simon.leneveu@gmail.com>
2024-09-12 22:59:47 +0200

    added help option

--- taskPlusPlus.cpp
+++ taskPlusPlus.cpp
@@ -2,12 +2,45 @@
 #include <vector>
 #include <string>
 
-class Task {
+class Task 
+{
     public:
         std::string description;
         int importance;
 };
+const std::string HELP_TEXT = "n(ew) - Make a new task \n\nl(ist) - List current tasks\n";
 
-int main() {
+std::string to_lower(std::string string)
+{
+    std::string lowered_string = string;
+    for (char& c : lowered_string)
+    {
+        if (c >= 'A' && c <= 'Z')
+            c += 32;
+    }
+    return lowered_string;
+}
+
+std::string main_input() 
+{
+    std::string action;
+    std::cout << "What would you like to do ? (h for help) : ";
+    std::cin >> action;
+    std::cout << std::endl;
+
+    return action;
+}
+
+void main_options(std::string action)
+{
+    if (to_lower(action) == "h")
+    {
+        std::cout << HELP_TEXT;
+    }
+}
+
+int main() 
+{
+    main_options(main_input());
     return 0;
 }
